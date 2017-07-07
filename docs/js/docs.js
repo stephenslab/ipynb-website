0526f181ed15a39355c944e1c73cdb828e0ac05b
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-07-07 16:13:20 -0500

    Re-build license.html.

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
-var licenseArray = ["license"]
-var licenseDict = {"l-source-code-and-software-in-this-repository-are-made-available-under-the-terms-of-the-1":"license"}
+var licenseArray = ["/opensource.org/licenses/mit-license"]
+var licenseDict = {}
 var analysisArray = ["Compute_PCs", "Plot_PCs"]
 var analysisDict = {"Computing-PCs-using-rsvd-1":"computepcs","Plotting-Iris-PCA-results-using-ggplot2-1":"plotpcs"}
