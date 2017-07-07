7528411b4be2c9c7d8425892472208911008abf6
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-07-07 16:53:27 -0500

    Deleted config.default.yml; fixed 2 broken links.

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
-var analysisArray = ["Compute_PCs", "Plot_PCs"]
+var analysisArray = ["computepcs", "plotpcs"]
 var analysisDict = {"Computing-PCs-using-rsvd-1":"computepcs","Plotting-Iris-PCA-results-using-ggplot2-1":"plotpcs"}
 var licenseArray = ["/opensource.org/licenses/mit-license"]
 var licenseDict = {}
