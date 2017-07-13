848eacd750e8704de8b8dc29dc8ab9f52839c695
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-07-13 10:52:23 -0500

    Release website after making the previous changes to the notebooks.

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
-var analysisArray = ["setup", "first-glance", "station-map", "time-of-day-trends", "seasonal-trends", "/github.com/jdblischak) and [Matthew Stephens](http://stephenslab.uchicago.edu) for their assistance and support. Also, thanks to [Larry Layne](https://rstudio-pubs-static.s3.amazonaws.com/63061_90f5136ffdf74740b6ba4ad8f2fd72fe"]
+var analysisArray = ["first-glance", "station-map", "time-of-day-trends", "seasonal-trends", "/github.com/jdblischak) and [Matthew Stephens](http://stephenslab.uchicago.edu) for their assistance and support. Also, thanks to [Larry Layne](https://rstudio-pubs-static.s3.amazonaws.com/63061_90f5136ffdf74740b6ba4ad8f2fd72fe"]
 var analysisDict = {"A-first-glance-at-the-Divvy-data-1":"first-glance","Divvy-usage-over-the-year-1":"seasonal-trends","A-map-of-the-Divvy-stations-in-Chicago-1":"station-map","Divvy-usage-by-time-of-day-1":"time-of-day-trends"}
 var setupArray = []
 var setupDict = {}
