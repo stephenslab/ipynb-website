aeb98e3ba39884cc435da4ac3421d177fcd44a18
Gao Wang <gaow@uchicago.edu>
2018-02-12 10:23:39 -0600

    Compile a bug test example

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,5 +1,5 @@
 var analysisArray = ['first-glance', 'station-map', 'time-of-day-trends', 'seasonal-trends']
-var analysisDict = {"A-first-glance-at-the-Divvy-data-1":"first-glance","Divvy-usage-over-the-year-1":"seasonal-trends","A-map-of-the-Divvy-stations-in-Chicago-1":"station-map","Divvy-usage-by-time-of-day-1":"time-of-day-trends"}
+var analysisDict = {"A-first-glance-at-the-Divvy-data-1":"first-glance","Divvy-usage-over-the-year-1":"seasonal-trends","A-map-of-the-Divvy-stations-in-Chicago-1":"station-map","Stationarity-distribution-of-the-discrete-time-stepping-stone-model-1":"stepping-stone-stationary","Divvy-usage-by-time-of-day-1":"time-of-day-trends"}
 var setupArray = []
 var setupDict = {}
 var licenseArray = []
